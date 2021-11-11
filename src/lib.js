805a6bc9080a628709567c09b4022659cff1cb5c
kolserdav <serega12101983@gmail.com>
2021-11-11 13:25:34 +0700

    Added failed to certbot logs

--- src/lib.js
+++ src/lib.js
@@ -251,7 +251,11 @@ module.exports = class Worker {
         }
       });
       sh.on('close', (code) => {
-        if (/Saving debug log/.test(errorData) && !/An unexpected error occurred/.test(errorData)) {
+        if (
+          /Saving debug log/.test(errorData) &&
+          !/error/.test(errorData) &&
+          !/failed/.test(errorData)
+        ) {
           console.info(this.info, Cyan, Dim, errorData, Reset);
           resolve(0);
         }
